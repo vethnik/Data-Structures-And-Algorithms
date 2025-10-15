4021a191ef71579f4435d51b4db302c9bc80d4e0
Melala Venkata Shiva Sai Vethnik <vethnik007@gmail.com>
2025-10-15 20:18:10 +0530

    5th pattern

---
+++ 1.Patterns/5th pattern.py
@@ -0,0 +1,10 @@
+class Solution:
+    def printTriangle(self, N):
+        # Code here
+        for  i in range(0,N):
+            for j in range(1,N-i+1):
+                print("* ",end="")
+            print()
+N=int(input())
+obj=Solution()
+obj.printTriangle(N)
